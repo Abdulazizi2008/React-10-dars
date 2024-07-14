011622e65f02052880f4732116d8f1e74b4b4a77
Abdulazizi2008 <ozodbekovabdu@gmail.com>
2024-07-15 00:15:54 +0500

    done

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      className="bg-black text-white text-center py-[16px]
+    mt-[80px]"
+    >
+      Copyright 2021. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
